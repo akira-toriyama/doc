fb17cf9e58f4c1df9feb0dc0dcccb53963831d3c
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-01-10 22:25:36 +0900

    fix promise

--- promise/cheatSheet.ts
+++ promise/cheatSheet.ts
@@ -1,8 +1,9 @@
-const asyncFn1 = () => Promise.resolve();
-const asyncFn2 = () => Promise.resolve();
-const asyncFn3 = () => Promise.resolve();
-const asyncFn4 = () => Promise.resolve();
-const asyncFn5 = () => Promise.resolve();
+const asyncFn1 = () => Promise.resolve("asyncFn1");
+const asyncFn2 = () => Promise.resolve("asyncFn2");
+const asyncFn3 = () => Promise.resolve("asyncFn3");
+const asyncFn4 = () => Promise.resolve("asyncFn4");
+const asyncFn5 = () => Promise.resolve("asyncFn5");
+const asyncFnX = () => Promise.reject(new Error("x"));
 
 /**
  * 一貫性のあるchain
@@ -38,6 +39,54 @@ const m3 = () => {
       // 下記のどちらかの方法でcatchに流れる
       // throw new Error("error");
       // return Promise.reject(new Error("error"));
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+/**
+ * Promise.all
+ *
+ * 1つでもエラーならば、catchへ
+ * 他の結果を利用して、使用したいに使用する。
+ *
+ * 下記例だと、asyncFnXがあるのでcatchへ
+ */
+const m4 = () => {
+  Promise.all([asyncFn1(), asyncFn2(), asyncFnX()])
+    .then((r) => {
+      console.log(r);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+/**
+ * Promise.allSettled
+ *
+ * かならず、thenへ
+ * 他の結果を使用せず、楽観的な実行
+ *
+ * 下記例だと、asyncFnXがあるがthenへ流れる
+ */
+const m5 = () => {
+  Promise.allSettled([asyncFn1(), asyncFn2(), asyncFnX()]).then((r) => {
+    console.log(r);
+  });
+};
+
+/**
+ * Promise.any
+ *
+ * どれか1つでも成功した場合は、thenへ
+ * すべて失敗した場合は、集約されたエラーが catchへ
+ */
+const m6 = () => {
+  Promise.any([asyncFn1(), asyncFn2(), asyncFnX()])
+    .then((r) => {
+      console.log(r);
     })
     .catch((e) => {
       console.log(e);
